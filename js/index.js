0e156e25168880f488e2463e90e8fbdf52f1c614
Matheus Silva <matheushds.br@gmail.com>
2021-06-20 12:45:54 -0300

    feat: remove functions from global scope

--- js/index.js
+++ js/index.js
@@ -1,26 +1,28 @@
-const scrollSmooth = (selector) => {
-  $(selector).click(function(event){
+(() => {
+  const scrollSmooth = (selector) => {
+    $(selector).click(function(event){
+      event.preventDefault();
+
+      const target = $(this).attr('href');
+
+      $('html, body').animate({
+        scrollTop: $(target).offset().top
+      }, 1000);
+    });
+  }
+
+  scrollSmooth('a[href*=panel-about]');
+  scrollSmooth('a[href*=panel-speakers]');
+  scrollSmooth('a[href*=panel-form]');
+
+  const showAlert = (event) => {
     event.preventDefault();
 
-    const target = $(this).attr('href');
+    const name = document.querySelector('[data-input-name]').value;
+    const email = document.querySelector('[data-input-email]').value;
+    alert(`Olá, ${name}! Seus dados foram enviados com sucesso! \n E-mail: ${email}`);
+  }
 
-    $('html, body').animate({
-      scrollTop: $(target).offset().top
-    }, 1000);
-  });
-}
-
-scrollSmooth('a[href*=panel-about]');
-scrollSmooth('a[href*=panel-speakers]');
-scrollSmooth('a[href*=panel-form]');
-
-const showAlert = (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('[data-input-name]').value;
-  const email = document.querySelector('[data-input-email]').value;
-  alert(`Olá, ${name}! Seus dados foram enviados com sucesso! \n E-mail: ${email}`);
-}
-
-const submitForm = document.querySelector('[data-submit-form]');
-submitForm.addEventListener('submit', showAlert);
+  const submitForm = document.querySelector('[data-submit-form]');
+  submitForm.addEventListener('submit', showAlert);
+})()
